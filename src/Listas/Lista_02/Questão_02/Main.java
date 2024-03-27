0e7a5f7ebeb5f1c495ed7cd14b4022922026ccc0
LeoFhin <leonardo.santos7507@gmail.com>
2024-03-27 00:55:18 -0300

    Lista 02 java

---
+++ src/Listas/Lista_02/Questão_02/Main.java
@@ -0,0 +1,27 @@
+package Listas.Lista_02.Questão_02;
+import java.util.Scanner;
+public class Main {
+    public static void main(String [] args){
+
+        Scanner sc = new Scanner(System.in);
+        Estoque etq = new Estoque();
+
+        System.out.println("Dígite a Quantidade Atual do estoque: ");
+        etq.setQtd_atual(sc.nextInt());
+
+        System.out.println("Dígite a Quantidade Maxíma do estoque: ");
+        etq.setQtd_maxima(sc.nextInt());
+
+        System.out.println("Digite a Quantidade Mínima do estoque: ");
+        etq.setQtd_minima(sc.nextInt());
+
+        etq.qtd_media();
+
+        if(etq.getQtd_atual() >= etq.getMedia()) {
+            System.out.println("Não efetuar compra!");
+        }
+        else {
+            System.out.println("Efetuar compra!");
+        }
+    }
+}
